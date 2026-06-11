7b7ea06c277083320c2d9af17721a9b37d5bd1d0
JoaoMarconi03 <jvmarconi03@gmail.com>
2026-06-11 15:50:14 +0000

    Nova estruturação do front end

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,10 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da media" });
+  const updatedAt = new Date().toLocaleString("pt-BR");
+  response.status(200).json({
+    data_atual: updatedAt,
+  });
 }
 
 export default status;
